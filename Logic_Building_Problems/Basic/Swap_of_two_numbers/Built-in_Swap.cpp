ac5ef682de3492666fd186ce310637df91ff2054
Mohammed Faisal Farooq <mdfaisalfarooq@gmail.com>
2026-02-28 14:36:02 +0530

    Add Built-in Swap program to swap two numbers

---
+++ Logic_Building_Problems/Basic/Swap_of_two_numbers/Built-in_Swap.cpp
@@ -0,0 +1,20 @@
+/*
+Problem: Swap Two Numbers
+Method:Built-in Swap
+
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 2, b = 3;
+    cout << "a = " << a << " b = " << b << endl;   
+  
+    swap(a, b);
+  
+    cout << "a = " << a << " b = " << b << endl;
+    return 0;
+}
