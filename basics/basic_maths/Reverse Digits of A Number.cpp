065d49896790a0c6aafcc5fea0ee3f58ef1bec4d
Mohammed Faisal Farooq <mdfaisalfarooq@gmail.com>
2026-06-27 10:50:35 +0530

    Add program to reverse digits of a number

---
+++ basics/basic_maths/Reverse Digits of A Number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{  int n;
+   cin>>n;
+   int rev;
+   int rem;
+   while(n!=0){
+       rem=n%10;
+       rev=rev*10+rem;
+       n=n/10;
+   }
+   cout<<rev;
+}
