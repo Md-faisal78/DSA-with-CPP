f0c1271c7b88ea2fb410902ea765deffda2c383c
Mohammed Faisal Farooq <mdfaisalfarooq@gmail.com>
2026-07-16 21:44:34 +0530

    Add optimal bitwise solution to reverse bits

---
+++ Bit_Manipulation/Easy/reverse_bits/optimal_bitwise.cpp
@@ -0,0 +1,25 @@
+// Reverse Bits - Bit Manipulation (O(32))
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;          // shift result left
+            result |= (n & 1);     // add last bit of n
+            n >>= 1;               // shift n right
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    uint32_t n = 43261596;
+    Solution obj;
+    cout << obj.reverseBits(n);
+}
