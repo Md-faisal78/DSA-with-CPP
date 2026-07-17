6002ad8df870a6f39f758d217e26fb5a9f13cb55
Mohammed Faisal Farooq <mdfaisalfarooq@gmail.com>
2026-07-17 17:52:23 +0530

    Add optimal DP solution for Word Break problem

---
+++ Dynamic_Programming/Medium/word_break/optimal_dp.cpp
@@ -0,0 +1,36 @@
+// Word Break - DP (O(n^2))
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        int n = s.size();
+
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
+
+int main() {
+    string s = "leetcode";
+    vector<string> dict = {"leet", "code"};
+
+    Solution obj;
+    cout << obj.wordBreak(s, dict);
+}
